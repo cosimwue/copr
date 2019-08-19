e76905ab566afd772031f2b62914076b9b3bdee2
severinsimmler <severin.simmler@posteo.de>
2019-08-19 17:32:16 +0200

    feat: game

---
+++ src/app/components/battle/battle.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Settings } from '../../models';
+
+@Component({
+  selector: 'app-battle',
+  templateUrl: './battle.component.html',
+  styleUrls: ['./battle.component.scss']
+})
+export class BattleComponent {
+  @Input('settings') settings: Settings;
+
+  constructor(private modalService: NgbModal) { }
+
+  open(content: any) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+}
--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,8 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ElectronService } from '../../providers/electron.service';
-import { Unit } from '../../models';
+import { Unit, Settings, Army } from '../../models';
 import { Router } from '@angular/router';
+
+var single = [
+  {
+    "name": "Germany",
+    "value": 8940000
+  },
+  {
+    "name": "USA",
+    "value": 5000000
+  },
+  {
+    "name": "France",
+    "value": 7200000
+  }
+];
+
+var multi = [
+  {
+    "name": "Germany",
+    "series": [
+      {
+        "name": "2010",
+        "value": 7300000
+      },
+      {
+        "name": "2011",
+        "value": 8940000
+      }
+    ]
+  },
+
+  {
+    "name": "USA",
+    "series": [
+      {
+        "name": "2010",
+        "value": 7870000
+      },
+      {
+        "name": "2011",
+        "value": 8270000
+      }
+    ]
+  },
+
+  {
+    "name": "France",
+    "series": [
+      {
+        "name": "2010",
+        "value": 5000002
+      },
+      {
+        "name": "2011",
+        "value": 5800000
+      }
+    ]
+  }
+];
+
+
+
 
 @Component({
   selector: 'app-game',
@@ -15,13 +77,35 @@ export class GameComponent implements OnInit {
   public settings: any;
   public countdownConfig: any;
 
+  view: any[] = [800, 400];
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // line, area
+  autoScale = true;
+
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();
     this.settings = navigation.extras.state as {
       test: string
     };
+
+
+    Object.assign(this, {single, multi})
   }
 
+  /*
   public timeSteps(n: number): Array<number> {
     return Array.from(Array(n).keys());
   }
@@ -76,12 +160,18 @@ export class GameComponent implements OnInit {
         break;
       }
     }
-  }
+  }*/
 
   public ngOnInit(): void {
-    this.a = new Unit('England', 500, 0.7);
+    let armyA = new Army('England', 1000);
+    let armyB = new Army('France', 1000);
+    let law = 'lanchester';
+    let implementation = 'battle(attacker: Unit, attacked: Unit): void {\n    // Write your code in this block, for example:\n    attacked.size = attacked.size - attacker.force;\n}';
+    this.settings = new Settings(armyA, armyB, law, implementation);
+
+    /*this.a = new Unit('England', 500, 0.7);
     this.b = new Unit('France', 800, 0.6);
-    this.countdownConfig = {'leftTime': 120, 'template': '$!m!:$!s!'}
+    this.countdownConfig = {'leftTime': 120, 'template': '$!m!:$!s!'}*/
   }
 
 }
